69a20345231f951e0ab796df441afa1380655d68
KevinPilkerton <kpilke1@students.towson.edu>
2017-02-16 20:04:52 -0500

    Task 14 & 15 Complete
    Task 14 & 15 Complete

--- src/edu/towson/cis/cosc442/project1/monopoly/GoToJailCell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GoToJailCell.java
@@ -11,4 +11,14 @@ public class GoToJailCell extends Cell {
 		GameMaster.instance().getGameBoard().queryCell("Jail");
 		GameMaster.instance().sendToJail(currentPlayer);
 	}
+
+	public void playerMoved(Player player, int playerIndex, GameMaster gameMaster) {
+		if (this.isAvailable()) {
+			int price = this.getPrice();
+			if (price <= player.getMoney() && price > 0) {
+				gameMaster.getGUI().enablePurchaseBtn(playerIndex);
+			}
+		}
+		gameMaster.getGUI().enableEndTurnBtn(playerIndex);
+	}
 }
--- src/edu/towson/cis/cosc442/project1/monopoly/UtilityCell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/UtilityCell.java
@@ -33,4 +33,14 @@ public class UtilityCell extends Cell {
 			}
 		}
 	}
+
+	public void playerMoved(Player player, int playerIndex, GameMaster gameMaster) {
+		if (this.isAvailable()) {
+			int price = this.getPrice();
+			if (price <= player.getMoney() && price > 0) {
+				gameMaster.getGUI().enablePurchaseBtn(playerIndex);
+			}
+		}
+		gameMaster.getGUI().enableEndTurnBtn(playerIndex);
+	}
 }
